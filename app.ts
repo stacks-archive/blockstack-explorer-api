dc52a6c645a25759a859283ea793ed2d6373712b
Hank Stoever <hstove@gmail.com>
2019-10-25 10:29:37 -0700

    prometheus server setup

--- app.ts
+++ app.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import * as Sentry from '@sentry/node';
+import { createMiddleware as createPrometheusMiddleware } from '@promster/express';
+import { createServer } from '@promster/server';
+// const { createMiddleware: createPrometheusMiddleware } = require('@promster/express');
+// const { createServer } = require('@promster/server');
 
 import makeAPIController from './controllers/api-controller';
 import V2ApiController from './controllers/v2-controller';
@@ -22,6 +26,11 @@ const getApp = async () => {
   if (process.env.SENTRY_DSN) {
     app.use(Sentry.Handlers.requestHandler());
   }
+
+  app.use(createPrometheusMiddleware({ app }));
+
+  // Create `/metrics` endpoint on separate server
+  createServer({ port: 9152 }).then(() => console.log('@promster/server started on port 9152.'));
 
   app.get('/', () => {
     throw new Error('Home page is not available.');
