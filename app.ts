0b4a3f6f2dd7d094992eca3c99fab629a7b906e7
Hank Stoever <hstove@gmail.com>
2019-10-25 11:22:23 -0700

    ignore 'account not found' error insentry

--- app.ts
+++ app.ts
@@ -13,6 +13,9 @@ if (process.env.SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     environment: process.env.SENTRY_ENV || 'production',
+    ignoreErrors: [
+      'Account not found',
+    ],
   });
 }
 
